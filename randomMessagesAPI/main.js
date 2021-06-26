d53ce495fd99ec2b0c782f35e0609fa94d3909d8
migben <itsmigben@gmail.com>
2021-06-26 10:11:58 -0400

    applied basic css styling to our api test

--- randomMessagesAPI/main.js
+++ randomMessagesAPI/main.js
@@ -1,11 +1,26 @@
 const btn = document.getElementById('activity')
+const idea = document.getElementById('idea')
 
 btn.addEventListener('click',function(){
     fetch('https://www.boredapi.com/api/activity')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-
-        document.getElementById('idea').textContent = data.activity
+        // console.log(data)
+        idea.textContent = data.activity
     })
+
+    const randColor = Math.floor(Math.random()*16777215).toString(16)
+    idea.style.color = "#"+ randColor
+    idea.style.backgroundColor = "#fff"
+    
 })
+
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  genNew.addEventListener("click", setBg);
+  setBg();
