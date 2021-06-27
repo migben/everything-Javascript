20c0a979a658c64e31ef8446588f46aa2237d513
migben <itsmigben@gmail.com>
2021-06-27 07:07:10 -0400

    started a small blog with the help of a dummy data API & basic styling

---
+++ simpleBlog/main.js
@@ -0,0 +1,20 @@
+const blogList = document.getElementById("blog-list")
+
+
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(res => res.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        // console.log(postsArr)
+        let html = ""
+
+        for(let post of postsArr){
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        blogList.innerHTML = html
+    })
