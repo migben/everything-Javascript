00886aea41e652990224823f1a7a475aa5bc8789
migben <itsmigben@gmail.com>
2021-06-28 07:07:56 -0400

    adding a new post

--- simpleBlog/main.js
+++ simpleBlog/main.js
@@ -1,6 +1,6 @@
 const blogList = document.getElementById("blog-list")
-
-
+const btn = document.getElementById("btn")
+const newPost = document.getElementById("new-post")
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
@@ -18,3 +18,35 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         }
         blogList.innerHTML = html
     })
+
+newPost.addEventListener("submit", function(e){ // listening for the form submit
+    e.preventDefault()
+
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
+    }
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            blogList.innerHTML = `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+                ${blogList.innerHTML}
+            `
+        })
+})
