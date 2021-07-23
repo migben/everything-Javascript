24ca68867b3c295e36585f12c4796a47a9ae2166
migben <itsmigben@gmail.com>
2021-07-23 13:24:09 -0400

    progress bar consistently updated

--- musicPlayer/main.js
+++ musicPlayer/main.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img")
 const title = document.getElementById("title")
 const artist = document.getElementById("artist")
 const music = document.querySelector('audio')
+const progressContainer = document.getElementById("progress-container")
+const progress = document.getElementById("progress")
 const prevBtn = document.getElementById("prev")
 const playBtn = document.getElementById("play")
 const nextBtn = document.getElementById("next")
@@ -90,7 +92,20 @@ function nextSong(){
 
 loadSong(songs[songIndex]) 
 
+
+// update progress and time
+function updateProgressBar(e) {
+    if(isPlaying) {
+        const {duration, currentTime} = e.srcElement
+        // updating the progress bar width
+        const progressPercent = (currentTime/duration) * 100
+        progress.style.width = `${progressPercent}%`
+    }
+}
+
 // Event listeners
 
 prevBtn.addEventListener("click", prevSong)
 nextBtn.addEventListener("click", nextSong)
+
+music.addEventListener("timeupdate", updateProgressBar)
